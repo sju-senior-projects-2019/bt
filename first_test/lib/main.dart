56b11f92268eebd84ecdf713980e4548648acc8d
Brett R Tillotson <44848617+CommanderFunion@users.noreply.github.com>
2019-03-19 16:24:01 -0400

    Add files via upload

--- first_test/lib/main.dart
+++ first_test/lib/main.dart
@@ -7,20 +7,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pacman Game',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Pacman')),
+        body: Column(
+          children: <Widget>[
+            Image.asset(
+              'sprites/maze-1.png',
+              scale: 0.5,
+            ),
+            Image.asset(
+              'sprites/noun-up.png',
+              scale: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'sprites/noun-left.png',
+                  scale: 10.0,
+                ),
+                Spacer(),
+                Image.asset(
+                  'sprites/noun-right.png',
+                  scale: 10.0,
+                )
+              ]
+            ),
+            Image.asset(
+              'sprites/noun-down.png',
+              scale: 10.0,
+            )
+          ]
+        )
+      ),
     );
   }
 }
